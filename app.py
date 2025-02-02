fbbf99875e1c1982c914afc59603916af5272b2f
Santiago Montealegre Largo <santiago.mon.1996@gmail.com>
2025-02-02 22:42:03 +0200

    Go back to the old interface

--- app.py
+++ app.py
@@ -76,7 +76,6 @@ def equipment_page(equipment_id):
     )
 
 
-
 @app.route('/delete_reservation/<int:reservation_id>', methods=['POST'])
 def delete_reservation(reservation_id):
     reservation = Reservation.query.get_or_404(reservation_id)
